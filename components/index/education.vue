0be940bbb81677354c22b768827f94a56a3dcdf3
Brian Adams <albashiroh.programming@gmail.com>
2024-01-18 11:33:03 +0700

    create education component

---
+++ components/index/education.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="min-h-screen py-12" id="education">
+    <div class="rounded-2xl border border-neutral text-xs w-fit px-4 py-2 flex gap-2 items-center uppercase">
+
+      <LucideUserRound :size="16" />
+
+      Education
+    </div>
+    <p
+      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+      necessitatibus facere praesentium modi ratione cupiditate optio ea
+      accusamus, nobis voluptates? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Explicabo, placeat!</p
+    >
+  </div>
+</template>
