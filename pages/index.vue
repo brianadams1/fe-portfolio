39f756ef8848271be67bf7d4c29946ca4ce12537
Brian Adams <albashiroh.programming@gmail.com>
2024-01-18 08:39:24 +0700

    add grid

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,22 @@
 <template>
-  <!-- PROFILE CARD -->
-  <div class="col-span-10 md:col-span-3 min-h-screen grid">
-  <IndexProfilecard/>
-  </div>
-
-  <!-- SECTIONS -->
-  <div class="col-span-10 md:col-span-7">
-    <IndexHome />
-    <IndexProfile />
-    <IndexProjects />
-    <IndexAbout />
-    <IndexSkills />
-    <IndexPortfolio />
-    <IndexTestimonial />
-    <IndexContact />
+  <div class="grid grid-cols-10">
+    
+    <!-- PROFILE CARD -->
+    <div class="col-span-10 md:col-span-3 min-h-screen grid">
+    <IndexProfilecard/>
+    </div>
+  
+    <!-- SECTIONS -->
+    <div class="col-span-10 md:col-span-7">
+      <IndexHome />
+      <IndexProfile />
+      <IndexProjects />
+      <IndexAbout />
+      <IndexSkills />
+      <IndexPortfolio />
+      <IndexTestimonial />
+      <IndexContact />
+    </div>
   </div>
 </template>
 <!-- <script setup></script>
