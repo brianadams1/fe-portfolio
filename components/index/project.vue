94cf5c19175d88b067d890cf722812bbb8c2dceb
Brian Adams <albashiroh.programming@gmail.com>
2024-01-18 11:33:33 +0700

    create project component

---
+++ components/index/project.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen py-12" id="project">
+    <div class="rounded-2xl border border-neutral text-xs w-fit px-4 py-2 flex gap-2 items-center uppercase">
+      <LucideLayoutPanelLeft :size="12"/>
+      Project
+</div>
+    <p
+      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque dolor
+      magni ratione nisi voluptas iusto assumenda cupiditate perspiciatis
+      dignissimos quidem.</p
+    >
+  </div>
+</template>
