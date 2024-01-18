39b175e07dbd7c017592109ea90da457ad1eb04f
Brian Adams <albashiroh.programming@gmail.com>
2024-01-18 11:33:18 +0700

    edit home component

--- components/index/home.vue
+++ components/index/home.vue
@@ -1,5 +1,15 @@
 <template>
-    <div class="min-h-screen" id="home"> Home 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque dolor magni ratione nisi voluptas iusto assumenda cupiditate perspiciatis dignissimos quidem.</p>
-      </div>
+  <div class="min-h-screen py-12" id="home">
+    <div
+      class="rounded-2xl border border-neutral text-xs w-fit px-4 py-2 flex gap-2 items-center uppercase"
+    >
+      <LucideHome :size="12" />
+      introduce
+    </div>
+    <p class="lg:text-6xl xl:text-7xl font-light mt-20 leading-tight">
+      Say Hi from <span class="text-info">Drake</span>, Webflow Designer and
+      Developer
+    </p>
+    <p class="mt-10 text-white/25">I design and code beautifully simple things and i love what i do. Just simple like that!</p>
+  </div>
 </template>
