51cb1e36a7b77ceca5803604e8c1e223c9dc8cd0
Brian Adams <albashiroh.programming@gmail.com>
2024-01-18 10:11:27 +0700

    style about

--- components/index/about.vue
+++ components/index/about.vue
@@ -1,16 +1,26 @@
 <template>
-    <div class="min-h-screen py-12" id="about"> 
-        <div class="rounded-2xl border border-neutral w-fit px-4 py-1 flex gap-2 items-center">
-            <LucideBarChartHorizontal :size="16"/>
+  <div class="min-h-screen py-12" id="about">
+    <div
+      class="rounded-2xl border border-neutral w-fit px-4 py-1 flex gap-2 items-center"
+    >
+      <LucideBarChartHorizontal :size="16" />
 
-            About
-        </div>
-        <div class="flex flex-col gap-6 mt-10">
-            <p class="text-4xl font-semibold">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, vitae.</p>
-            <p class="font-light">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam illo fuga natus ab ut similique neque veritatis harum, saepe fugit eius qui nam ex quam magnam doloribus praesentium doloremque esse quis ea. Obcaecati dolorum magni fugit voluptatem, nisi labore beatae nobis impedit inventore, ratione mollitia exercitationem possimus quos quam hic!</p>
-        </div>
+      About
     </div>
+    <div class="flex flex-col gap-6 mt-10">
+      <p class="text-5xl font-semibold"
+        >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente,
+        vitae.</p
+      >
+      <p class="font-light"
+        >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam illo
+        fuga natus ab ut similique neque veritatis harum, saepe fugit eius qui
+        nam ex quam magnam doloribus praesentium doloremque esse quis ea.
+        Obcaecati dolorum magni fugit voluptatem, nisi labore beatae nobis
+        impedit inventore, ratione mollitia exercitationem possimus quos quam
+        hic!</p
+      >
+    </div>
+  </div>
 </template>
-<script setup>
-
-</script>
+<script setup></script>
