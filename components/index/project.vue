24fa1cba23e6d4f7c3a67943c7d93b9113d482b0
Brian Adams <albashiroh.programming@gmail.com>
2024-01-18 11:51:45 +0700

    edit project component

--- components/index/project.vue
+++ components/index/project.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="min-h-screen py-12" id="project">
-    <div class="rounded-2xl border border-neutral text-xs w-fit px-4 py-2 flex gap-2 items-center uppercase">
-      <LucideLayoutPanelLeft :size="12"/>
-      Project
-</div>
-    <p
-      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque dolor
-      magni ratione nisi voluptas iusto assumenda cupiditate perspiciatis
-      dignissimos quidem.</p
+    <div
+      class="rounded-2xl border border-neutral text-xs w-fit px-4 py-2 flex gap-2 items-center uppercase"
     >
+      <LucideLayoutPanelLeft :size="12" />
+      <div> Project </div>
+    </div>
+    <div class="mt-10">
+      <div class="text-4xl font-semibold">Latest Projects</div>
+      <!-- list project -->
+      <div class="grid grid-cols-2  gap-6 mt-8">
+        <!-- project item -->
+        <div class="col-span-2 md:col-span-1" v-for="p in 4">
+          <div class="flex justify-between items-end">
+            <div class="font-semibold text-lg text-secondary uppercase">Judul Project</div>
+            <div class="text-sm ">Jan 20th, 2020</div>
+          </div>
+          <!-- image -->
+          <div class="aspect-video bg-neutral rounded-xl"></div>
+          <!-- skill -->
+          <div class="flex gap-2 flex-nowrap overflow-hidden ">
+            <div v-for="n in 10" class="border-neutral border rounded-xl px-3 mt-2 text-nowrap">Skill {{ n }}</div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
   </div>
 </template>
