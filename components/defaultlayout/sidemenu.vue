fda3afc4754266651608553cc97e4a07addbfa65
Brian Adams <albashiroh.programming@gmail.com>
2024-01-19 11:59:49 +0700

    spacing edit

--- components/defaultlayout/sidemenu.vue
+++ components/defaultlayout/sidemenu.vue
@@ -1,28 +1,28 @@
 <template>
-     <div class="min-h-screen w-28 flex-none max-md:hidden">
-      <div class="fixed min-h-screen flex items-center">
-        <div
-          class="icons rounded-full border-2 border-neutral p-5 flex flex-col gap-6 text-center"
+  <div class="min-h-screen w-28 flex-none max-md:hidden">
+    <div class="fixed min-h-screen flex items-center">
+      <div
+        class="icons rounded-full border-2 border-neutral p-5 flex flex-col gap-6 text-center"
+      >
+        <NuxtLink
+          v-for="menu in menus"
+          :to="{ path: menu.path, hash: menu.hash }"
+          class="tooltip tooltip-left cursor-pointer"
+          :data-tip="menu.title"
         >
-          <NuxtLink
-            v-for="menu in menus"
-            :to="{ path: menu.path, hash: menu.hash }"
-            class="tooltip tooltip-left cursor-pointer"
-            :data-tip="menu.title"
-          >
-            <component
-              :is="menu.icon"
-              :size="18"
-              class="text-white hover:text-accent"
-            />
-          </NuxtLink>
-        </div>
+          <component
+            :is="menu.icon"
+            :size="18"
+            class="text-white hover:text-accent"
+          />
+        </NuxtLink>
       </div>
     </div>
+  </div>
 </template>
 
 <script setup>
 defineProps({
-    menus: Array
-})
+  menus: Array,
+});
 </script>
