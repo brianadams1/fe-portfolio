50a31f89dd8b93f5b1ba38f6c21edac94bb7d9cb
Brian Adams <albashiroh.programming@gmail.com>
2024-01-19 09:57:17 +0700

    edit skill section

--- components/index/skill.vue
+++ components/index/skill.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="min-h-screen py-12" id="skill">
-    <div class="rounded-2xl border border-neutral text-xs w-fit px-4 py-2 flex gap-2 items-center uppercase">
-      <LucideWrench :size="12"/>
-      Skill
-</div>
-    <p
-      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque dolor
-      magni ratione nisi voluptas iusto assumenda cupiditate perspiciatis
-      dignissimos quidem.</p
+    <div
+      class="rounded-2xl border border-neutral text-xs w-fit px-4 py-2 flex gap-2 items-center uppercase"
     >
+      <LucideWrench :size="13" />
+      <div> Skill </div>
+    </div>
+    <div class="mt-10">
+      <p class="text-4xl font-semibold mb-6">
+        Every great <span class="text-info">Projects</span> begin with skills to <span class="text-accent">Learn</span>
+      </p>
+      <div v-for="m in 4" class="mb-10">
+        <div class="font-semibold uppercase text-secondary mb-2"
+          >SKILL CATEGORY</div
+        >
+        <div class="flex gap-6 flex-wrap">
+          <div v-for="n in 10">
+            <div
+              class="aspect-square bg-neutral w-16 rounded-md flex items-center justify-center"
+            >
+              <LucideWrench :size="28" />
+            </div>
+            <div class="text-center"> Skill {{ n }} </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
