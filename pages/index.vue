18459319f8c25f3a60b668488d10a3599922003a
Brian Adams <albashiroh.programming@gmail.com>
2024-01-26 07:20:57 +0700

    section object binding

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
     <div
       class="col-span-10 md:col-span-5 lg:col-span-4 xl:col-span-3 min-h-screen grid"
     >
-      <IndexProfilecard />
+      <IndexProfilecard :profile="profile" />
     </div>
 
     <!-- SECTIONS -->
@@ -13,17 +13,23 @@
     >
       <IndexHome />
       <IndexAbout />
-      <IndexBlog />
-      <IndexExperience />
-      <IndexEducation />
-      <IndexProject />
-      <IndexSkill />
+      <IndexBlog :blog="blogs"/>
+      <IndexExperience :experience="experience"/>
+      <IndexEducation :educations="educations"/>
+      <IndexProject :projects="projects"/>
+      <IndexSkill :skills="skills"/>
     </div>
   </div>
 </template>
 <script setup>
-console.log("Load Dataa================");
-const datas = await useFetch("http://localhost:5000/portfolio");
+const data = await $fetch("http://localhost:5000/portfolio");
+const profile = data.data.profile;
+console.log(profile)
+const experience = data.data.experience;
+const projects = data.data.projects;
+const skills = data.data.skills
+const educations = data.data.educations;
+const blogs = data.data.blogs;
 </script>
 
 <style scoped></style>
