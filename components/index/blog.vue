a63eb4bccf96012f1f0d4cb5c0b5f09f170bc704
Brian Adams <albashiroh.programming@gmail.com>
2024-01-26 08:24:02 +0700

    declare props blogs

--- components/index/blog.vue
+++ components/index/blog.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen py-12" id="blog">
     <div
-      class="rounded-2xl border border-neutral text-xs w-fit px-4 py-2 flex gap-2 items-center uppercase"
+      class="rounded-2xl border border-neutral text-xs w-fit px-8 py-2 flex gap-2 items-center uppercase"
     >
       <LucideSend :size="16" />
 
@@ -11,8 +11,7 @@
       <!-- blog list -->
       <div
         class="grid grid-cols-10 gap-3 group cursor-pointer hover:bg-secondary rounded-xl p-4"
-        v-for="n in 4"
-        :key="n"
+        v-for="blog in blogs"
       >
         <!-- image -->
         <div
@@ -20,16 +19,18 @@
         ></div>
         <div class="col-span-6 xl:col-span-7">
           <!-- title -->
-          <div class="text-2xl font-semibold group-hover:text-accent"
-            >Judul Blog</div
-          >
-          <div class="line-clamp-1 lg:line-clamp-2 xl:line-clamp-3"
-            >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
-            voluptates ipsam ducimus, perspiciatis nisi necessitatibus soluta
-            esse? Excepturi, nisi laborum.</div
-          >
+          <div class="text-2xl font-semibold group-hover:text-accent">{{
+            blog.title
+          }}</div>
+          <div class="line-clamp-1 lg:line-clamp-2 xl:line-clamp-3">{{
+            blog.content
+          }}</div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+defineProps({ blogs: Array });
+</script>
