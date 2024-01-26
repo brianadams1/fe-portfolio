0a41e7aab2a453be63396403bb61dc6477ecf022
Brian Adams <albashiroh.programming@gmail.com>
2024-01-26 08:23:53 +0700

    declare props about

--- components/index/about.vue
+++ components/index/about.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="min-h-screen py-12 flex flex-col" id="about">
     <div
-      class="rounded-2xl border border-neutral text-xs w-fit px-4 py-2 flex gap-2 items-center uppercase"
+      class="rounded-2xl border border-neutral text-xs w-fit px-8 py-2 flex gap-2 items-center uppercase"
     >
       <LucideBarChartHorizontal :size="16" />
 
       About
     </div>
     <div class="flex flex-col gap-12 mt-10 grow">
-      <p class="lg:text-6xl xl:text-7xl leading-tight">
-        Say Hi from <span class="text-info">Drake</span>, Webflow Designer and
-        Developer
+      <p class="md:max-lg:text-5xl text-6xl xl:text-7xl leading-tight">
+        Say Hi from <span class="text-info">{{ profile.firstName }}</span
+        >, Webflow Designer and Developer
       </p>
-      <p class="font-light"
-        >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam illo
-        fuga natus ab ut similique neque veritatis harum, saepe fugit eius qui
-        nam ex quam magnam doloribus praesentium doloremque esse quis ea.
-        Obcaecati dolorum magni fugit voluptatem, nisi labore beatae nobis
-        impedit inventore, ratione mollitia exercitationem possimus quos quam
-        hic!</p
+      <p class="font-light text-white/25"
+        >Since beginning my journey as a freelance designer nearly 8 years ago,
+        I've done remote work for agencies, consulted for startups, and
+        collaborated with talented people to create digital products for both
+        business and consumer use. I'm quietly confident, naturally curious, and
+        perpetually working on improving my chopsone design problem at a
+        time.</p
       >
       <div class="flex gap-20 grow items-end pb-10">
         <div>
@@ -39,4 +39,8 @@
     </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+defineProps({
+  profile: Object,
+});
+</script>
