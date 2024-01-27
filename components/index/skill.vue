09fb89c027122ea7a1b144a67a2cc2815de2970f
Brian Adams <albashiroh.programming@gmail.com>
2024-01-27 10:09:46 +0700

    declare skills props

--- components/index/skill.vue
+++ components/index/skill.vue
@@ -1,31 +1,37 @@
 <template>
   <div class="min-h-screen py-12" id="skill">
     <div
-      class="rounded-2xl border border-neutral text-xs w-fit px-4 py-2 flex gap-2 items-center uppercase"
+      class="rounded-2xl border border-neutral text-xs w-fit px-8 py-2 flex gap-2 items-center uppercase"
     >
       <LucideWrench :size="13" />
       <div> Skill </div>
     </div>
     <div class="mt-10">
+      <!-- SUBTITLE -->
       <p class="text-4xl font-semibold mb-6">
-        Every great <span class="text-secondary">Projects</span> begin with skills to
-        <span class="text-accent">Learn</span>
+        Every great <span class="text-secondary">Projects</span> begin with
+
+        <span class="text-accent">Skills</span> to learn
       </p>
-      <div v-for="m in 4" class="mb-10">
-        <div class="font-semibold uppercase text-secondary mb-2"
-          >SKILL CATEGORY</div
-        >
+      <div v-for="s in skills" class="mb-10">
+        <!-- CATEGORY -->
+        <div class="font-semibold uppercase text-secondary mb-2">{{
+          s.title
+        }}</div>
         <div class="flex gap-6 flex-wrap">
-          <div v-for="n in 10" class="group">
+          <!-- SKILL LIST -->
+          <div v-for="n in s.skill" class="group">
+            <!-- SVG -->
             <div
               class="aspect-square bg-neutral w-16 rounded-md flex items-center justify-center group-hover:scale-110 duration-300"
             >
+              <!-- {{ n.svg }} -->
               <LucideWrench :size="28" />
             </div>
             <div
               class="text-center group-hover:text-accent text-sm text-nowrap"
             >
-              Skill {{ n }}
+              {{ n.title }}
             </div>
           </div>
         </div>
@@ -33,3 +39,8 @@
     </div>
   </div>
 </template>
+<script setup>
+defineProps({
+  skills: Array,
+});
+</script>
