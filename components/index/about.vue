98d739f07adaa3054d2cbb9324c689382bd2bb7c
Brian Adams <albashiroh.programming@gmail.com>
2024-01-27 11:29:45 +0700

    handle reactive year/month exp and project count

--- components/index/about.vue
+++ components/index/about.vue
@@ -10,21 +10,35 @@
     <div class="flex flex-col gap-12 mt-10 grow">
       <p class="md:max-lg:text-5xl text-6xl xl:text-7xl leading-tight">
         Say Hi from <span class="text-info">{{ profile.firstName }}</span
-        >,<br> {{profile.job}}
+        >,<br />
+        {{ profile.job }}
       </p>
-      <p class="font-light text-white/25"
-        >{{profile.bio}}</p
-      >
+      <p class="font-light text-white/25">{{ profile.bio }}</p>
       <div class="flex gap-20 grow items-end pb-10">
-        <div>
-          <div class="text-accent text-4xl font-semibold">10+</div>
+        <!-- BASED ON YEAR -->
+        <div v-if="profile.year_of_exp != 0">
+          <div class="text-accent text-4xl font-semibold"
+            >{{ profile.year_of_exp }} +</div
+          >
           <div class="uppercase"
             >years of <br />
             experience</div
           >
         </div>
+        <!-- BASED ON MONTH -->
+        <div v-else>
+          <div class="text-accent text-4xl font-semibold"
+            >{{ profile.month_of_exp }} +</div
+          >
+          <div class="uppercase"
+            >months of <br />
+            experience</div
+          >
+        </div>
         <div>
-          <div class="text-accent text-4xl font-semibold">10+</div>
+          <div class="text-accent text-4xl font-semibold"
+            >{{ profile.project_count }}+</div
+          >
           <div class="uppercase"
             >projects <br />
             completed</div
