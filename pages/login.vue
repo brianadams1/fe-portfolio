e5e2bcbd3fbbdad7558e9e11819363b0f0291ce0
Brian Adams <albashiroh.programming@gmail.com>
2024-02-06 12:10:38 +0700

    create login page

---
+++ pages/login.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex justify-center bg-white/50">
+    <div class="bg-rose-500"> ini login form </div>
+    <div>
+      <SvgLogin class="w-80" />
+    </div>
+  </div>
+</template>
+<script setup></script>
