0b5a32065eb4790e23422e79b1bda649c25f9c85
Brian Adams <albashiroh.programming@gmail.com>
2024-02-12 10:31:46 +0700

    add pinia module

--- middleware/profile.ts
+++ middleware/profile.ts
@@ -7,3 +7,12 @@ export default defineNuxtRouteMiddleware(async(to, from) => {
     useProfile.value = fetchProfile;
   }
 });
+
+ /*
+ 
+ fungsi state management: 
+ 1. share data antar page
+ 2. gaperlu fetch data yg sama
+ 3. membuat method di dalam state 
+ 
+ */ 
