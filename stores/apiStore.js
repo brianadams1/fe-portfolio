41b1942830d3bd87200711d9804433aa918f7383
Brian Adams <albashiroh.programming@gmail.com>
2024-02-16 09:35:19 +0700

    handle login and logout method

--- stores/apiStore.js
+++ stores/apiStore.js
@@ -13,14 +13,61 @@ export const useApiStore = defineStore("api", {
           headers: { "Content-Type": "application/json" },
           credentials: "include",
         });
-        return data
+        return data;
       } catch (error) {
         console.log(error);
       }
     },
     //post
+    async post(path, data){
+      // get api uri
+      const config = useRuntimeConfig();
+      const apiUri = config.public.apiUri;
+
+      const jsonData = JSON.stringify(data);
+      try {
+        const userData = await $fetch(apiUri + path, {
+          method: "POST",
+          body: jsonData,
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+        });
+        return userData
+      } catch (error) {
+        console.log(error)
+      }
+
+    },
     //put
     //patch
-    //delet
+    //delete
+    async delete(path) {
+      // const config = useRuntimeConfig();
+      // const apiUri = config.public.apiUri;
+      // try {
+      //   const data = await $fetch(apiUri + path, {
+      //     method: "GET",
+      //     headers: { "Content-Type": "application/json" },
+      //     credentials: "include",
+      //   });
+      //   return data
+      // } catch (error) {
+      //   console.log(error);
+      // }
+      // get api uri
+      const config = useRuntimeConfig();
+      const apiUri = config.public.apiUri;
+      try {
+        const data = await $fetch(apiUri + path, {
+          method: "DELETE",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+        });
+
+        return data
+      } catch (error) {
+        console.log(error)
+      }
+    },
   },
 });
--- stores/authStore.js
+++ stores/authStore.js
@@ -6,36 +6,19 @@ export const useAuthStore = defineStore("auth", {
     async getUser() {
       // get api uri
       const Api = useApiStore();
-      this.user = await Api.get("/user"); 
+      this.user = await Api.get("/user");
     },
-    async login(formData) {
-      // get api uri
-      const config = useRuntimeConfig();
-      const apiUri = config.public.apiUri;
+    async login(data) {
+      const Api = useApiStore();
 
-      //   convert to json
-      const data = JSON.stringify(formData);
+      this.user = await Api.post("/login", data);
 
-      const user = await $fetch(apiUri + "/login", {
-        method: "POST",
-        body: data,
-        headers: { "Content-Type": "application/json" },
-        credentials: "include",
-      });
-      this.user = user;
       // REDIRECT TO ADMIN HOMEPAGE
       navigateTo("/admin");
     },
     async logout() {
-      // get api uri
-      const config = useRuntimeConfig();
-      const apiUri = config.public.apiUri;
-
-      await $fetch(apiUri + "/logout", {
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" },
-        credentials: "include",
-      });
+      const Api = useApiStore();
+      await Api.delete("/logout");
 
       // REDIRECT TO LOGIN PAGE
       navigateTo("/admin/login");
