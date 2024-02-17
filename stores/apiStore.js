03323b3cc7739a29f4f771e7404a25d5ee3d7d44
Brian Adams <albashiroh.programming@gmail.com>
2024-02-17 09:36:27 +0700

    handle error fetch, handle put &patch

--- stores/apiStore.js
+++ stores/apiStore.js
@@ -15,7 +15,7 @@ export const useApiStore = defineStore("api", {
         });
         return data;
       } catch (error) {
-        console.log(error);
+        this.handleError(error);
       }
     },
     //post
@@ -34,11 +34,47 @@ export const useApiStore = defineStore("api", {
         });
         return userData;
       } catch (error) {
-        throw error;
+        this.handleError(error);
       }
     },
     //put
+    async put(path, data) {
+      // get api uri
+      const config = useRuntimeConfig();
+      const apiUri = config.public.apiUri;
+
+      const jsonData = JSON.stringify(data);
+      try {
+        const userData = await $fetch(apiUri + path, {
+          method: "PUT",
+          body: jsonData,
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+        });
+        return userData;
+      } catch (error) {
+        this.handleError(error);
+      }
+    },
     //patch
+    async patch(path, data) {
+      // get api uri
+      const config = useRuntimeConfig();
+      const apiUri = config.public.apiUri;
+
+      const jsonData = JSON.stringify(data);
+      try {
+        const userData = await $fetch(apiUri + path, {
+          method: "PATCH",
+          body: jsonData,
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+        });
+        return userData;
+      } catch (error) {
+        this.handleError(error);
+      }
+    },
     //delete
     async delete(path) {
       // get api uri
@@ -53,8 +89,24 @@ export const useApiStore = defineStore("api", {
 
         return data;
       } catch (error) {
-        console.log(error);
+        this.handleError(error);
       }
+    },
+    handleError(error) {
+      //create method to catch error so it can be used in all methods
+
+      // 401 unauthorized error
+      if (error.status == 401) {
+        const token = useCookie("token");
+        token = "";
+        //  throw to login
+        return navigateTo("/admin/login");
+      }
+      // except 401
+      throw createError({
+        statusCode: error.status || 500, // default code 500
+        statusMessage: error.message || "Internal Server Error...", // default message
+      });
     },
   },
 });
