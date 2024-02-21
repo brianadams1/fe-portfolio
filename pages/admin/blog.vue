0d01aec1bf1a5f79ffca5d88445c9f20ce71a6a7
Brian Adams <albashiroh.programming@gmail.com>
2024-02-21 20:50:04 +0700

    blog loop

--- pages/admin/blog.vue
+++ pages/admin/blog.vue
@@ -1,14 +1,37 @@
 <template>
-  <div> ini halaman blog </div>
-  <div>Counter : {{ Counter.count }}</div>
-  <button class="btn btn-accent" @click="Counter.add10"> add 10 </button>
+  <div class="flex justify-center">
+    <div
+      v-if="BlogStore.blog.length"
+      class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-10 mx-auto"
+    >
+      <div
+        class="card card-compact bg-base-100 shadow-xl mb-5 w-full"
+        v-for="b in BlogStore.blog"
+      >
+        <figure>
+          <img :src="apiUri + b.photos[0].path" alt="Shoes" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">{{ b.title }}</h2>
+          <p>{{ b.content }}</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Edit Content</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-// import {useCounterStore} from '../../stores/counterStore'
 definePageMeta({
   layout: "admin",
   middleware: ["auth"],
 });
-const Counter = useCounterStore();
+const config = useRuntimeConfig();
+const apiUri = config.public.apiUri;
+const BlogStore = useBlogStore();
+onBeforeMount(async () => {
+  if (BlogStore.blog == null) await BlogStore.get();
+});
 </script>
