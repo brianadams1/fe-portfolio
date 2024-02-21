75f7c6b2117d0b5f37bac02b8792dbf123ceac85
Brian Adams <albashiroh.programming@gmail.com>
2024-02-21 10:24:04 +0700

    create admin pages

---
+++ pages/admin/education.vue
@@ -0,0 +1,14 @@
+<template>
+    <div> ini halaman education </div>
+    <div>Counter : {{ Counter.count }}</div>
+    <button class="btn btn-accent" @click="Counter.add10"> add 10 </button>
+  </template>
+  
+  <script setup>
+  // import {useCounterStore} from '../../stores/counterStore'
+  definePageMeta({
+    layout: "admin",
+    middleware: ["auth"],
+  });
+  const Counter = useCounterStore();
+  </script>
